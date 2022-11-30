dc4727d804074118909cb0395bca9e95504a3712
DanMarzo <marzogildan@gmail.com>
2022-11-30 15:41:22 -0300

    Nova activity que ativa ao apertar o botão

---
+++ app/src/main/java/com/example/copadomundo/EscalacaoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.copadomundo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EscalacaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_escalacao)
+    }
+}
