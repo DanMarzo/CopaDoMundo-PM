7bd78a0b23cd2bec79e2113030296d0e39d0d792
DanMarzo <marzogildan@gmail.com>
2022-11-30 15:42:05 -0300

    Activiry para escalacao

--- app/src/main/java/com/example/copadomundo/MainActivity.kt
+++ app/src/main/java/com/example/copadomundo/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.copadomundo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.butao_Selecao)
+        button.setOnClickListener {
+            ExibirEscalacao()
+        }
+    }
+    private fun ExibirEscalacao(){
+        val intent = Intent(this, EscalacaoActivity::class.java)
+        startActivity(intent)
     }
 }
