8d8645bd7baf7e329e2ff5d25d1ffdc086fc253d
DanMarzo <marzogildan@gmail.com>
2022-12-05 00:46:12 -0300

    Criando metodo para devolução de uma lista com os nome e imagens

---
+++ app/src/main/java/com/example/copadomundo/Data/DataSource.kt
@@ -0,0 +1,39 @@
+package com.example.copadomundo.Data
+
+import com.example.copadomundo.Model.Copa
+import com.example.copadomundo.R
+import com.example.copadomundo.R.*
+
+class DataSource {
+    fun loadCopa() : List<Copa>
+    {
+        return listOf<Copa>(
+            Copa(string.Copa1, R.drawable.umherman),
+            Copa(string.Copa2, R.drawable.doisfelix),
+            Copa(string.Copa3, R.drawable.trespiero),
+            Copa(string.Copa4, R.drawable.quatrorobert),
+            Copa(string.Copa5, R.drawable.cincojose),
+            Copa(string.Copa6, R.drawable.seiswilliam),
+            Copa(string.Copa7, R.drawable.setepervis),
+            Copa(string.Copa8, R.drawable.oitocarlos),
+            Copa(string.Copa9, R.drawable.nove9ayrton),
+            Copa(string.Copa10, R.drawable.dezromario),
+            Copa(string.Copa11, R.drawable.umummichael),
+            Copa(string.Copa12, R.drawable.umdoismoises),
+            Copa(string.Copa13, R.drawable.umtresenner),
+            Copa(string.Copa14, R.drawable.umquatroxavier),
+            Copa(string.Copa15, R.drawable.umcincoangel),
+            Copa(string.Copa16, R.drawable.umseisjeremy),
+            Copa(string.Copa17, R.drawable.umseteangelo),
+            Copa(string.Copa18, R.drawable.umoitodiego),
+            Copa(string.Copa19, R.drawable.umnovegonzalo),
+            Copa(string.Copa20, R.drawable.doiszerojhegson),
+            Copa(string.Copa21, R.drawable.doisumalan),
+            Copa(string.Copa22, R.drawable.doisdoisalexander),
+            Copa(string.Copa23, R.drawable.doistresmoises),
+            Copa(string.Copa24, R.drawable.doisquatrodjorkaeff),
+            Copa(string.Copa25, R.drawable.doiscincojackson),
+            Copa(string.Copa26, R.drawable.doisseiskevin),
+        )
+    }
+}
