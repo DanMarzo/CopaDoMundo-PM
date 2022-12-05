3c60559f05c652e3e03407343bf3a12562175a24
DanMarzo <marzogildan@gmail.com>
2022-12-05 00:43:29 -0300

    Ativando adaptador para Recycler

--- app/src/main/java/com/example/copadomundo/EscalacaoActivity.kt
+++ app/src/main/java/com/example/copadomundo/EscalacaoActivity.kt
@@ -1,11 +1,27 @@
 package com.example.copadomundo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.example.copadomundo.Data.DataSource
+import com.example.copadomundo.adapter.ItemAdapter
 
 class EscalacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_escalacao)
+
+
+        val myDataSet = DataSource().loadCopa()
+        val recyclerView = findViewById<RecyclerView>(R.id.recicler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+
+        recyclerView.setHasFixedSize(true)
+
+
+
     }
+
 }
